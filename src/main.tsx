7ea61b8c99b23d06a240c1c3c9b9a6cb52d0c8ba
Tania Lapa <taniamiroshnuchenko@gmail.com>
2026-03-23 19:51:37 +0100

    components

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+
+
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <div>
+    <h1>Welcome to React</h1>
+    <p>
+      This is JSX — it looks like HTML, but it's not quite the same. It has its
+      own rules!
+    </p>
+  </div>
+);
+
