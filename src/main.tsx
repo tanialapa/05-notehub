778a7ff8b46f3b1d275691ec6efaa714bb18760a
Tania Lapa <taniamiroshnuchenko@gmail.com>
2026-04-09 12:49:36 +0200

    components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import App from './сomponents/App/App'
+import App from './components/App/App'
 import './index.css'
 
 const queryClient = new QueryClient()
